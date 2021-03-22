9dbf8dfc621c09adff8f91898470805167422846
Efrain May <efrain_may_75@hotmail.com>
2021-03-22 11:31:26 -0600

    separacion del componente de menu .dart /sections/topSection/components

--- lib/src/sections/topSection/top_section.dart
+++ lib/src/sections/topSection/top_section.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/constantes.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/src/sections/topSection/components/efraincode_picture.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/src/sections/topSection/components/logo_blur_titlte_box.dart';
+import 'package:portatofio_efraindrummer_flutterwebb/src/sections/topSection/components/menu.dart';
 
 class TopSection extends StatelessWidget {
 
@@ -39,76 +40,4 @@ class TopSection extends StatelessWidget {
       ),
     );
   }
-}
-
-class Menu extends StatefulWidget {
-  @override
-  _MenuState createState() => _MenuState();
-}
-
-class _MenuState extends State<Menu> {
-
-  int selectedIndex = 0;
-  int hoverIndex = 0;
-  List<String> menuItems = [
-    "Home",
-    "About",
-    "Services",
-    "Portafolio",
-    "Testimonial",
-    "Contact"
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding * 2.5),
-      constraints: BoxConstraints(maxWidth: 1110),
-      height: 100,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10)
-        )
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: List.generate(
-          menuItems.length, 
-          (index) => buildMenuItems(index)
-        ),
-      ),
-    );
-  }
-
-  Widget buildMenuItems(int index) => InkWell(
-    onTap: (){
-      setState(() {
-        selectedIndex = index;
-      });
-    },
-    child: Container(
-      constraints: BoxConstraints(minWidth: 122),
-      height: 100,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Text(
-            menuItems[index].toUpperCase(),
-            style: TextStyle(
-              fontSize: 22,
-              color: kTextColor
-            ),  
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: selectedIndex == index ? -2 : -32,
-            child: Image.asset("assets/images/Hover.png"),
-          )
-        ],
-      ),
-    ),
-  );
 }
