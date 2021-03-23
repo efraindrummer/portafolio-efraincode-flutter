8f4e3b3224ed340740ce759eda4704862f9c1eeb
Efrain May <efrain_may_75@hotmail.com>
2021-03-23 12:54:42 -0600

    box blur radius text

--- lib/src/sections/about/about.section.dart
+++ lib/src/sections/about/about.section.dart
@@ -17,6 +17,56 @@ class About extends StatelessWidget {
           Expanded(
             child: AboutTextSection(text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam, Lorem ipsum dolor sit amet, consectetur adipisicing elit.!",),
           ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            padding: EdgeInsets.all(kDefaultPadding),
+            height: 240,
+            width: 255,
+            decoration: BoxDecoration(
+              color: Color(0xFFF7E8FF),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Color(0xFF16BFFD),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                    color: Color(0xFF928DAB).withOpacity(0.25)
+                  )
+                ]
+              ),
+              child: Column(
+                children: <Widget>[
+                  Stack(
+                    children: [
+                      Text(
+                        "08", 
+                        style: TextStyle(
+                          fontSize: 100, 
+                          fontWeight: FontWeight.bold,
+                          foreground: 
+                            Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 6
+                            ..color = Color(0xFF2C3E50).withOpacity(0.5),
+                            shadows: [
+                              BoxShadow(
+                                offset: Offset(0, 5),
+                                blurRadius: 10,
+                                color: Color(0xFFA600FF).withOpacity(0.5)
+                              )
+                            ]      
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
           Expanded(
             child: AboutTextSection(text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam, Lorem ipsum dolor sit amet, consectetur adipisicing elit.!",),
           ),
