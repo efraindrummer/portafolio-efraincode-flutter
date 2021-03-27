5476f5f3f856d15b0a272f96e93f4a7c7cb4fc82
Efrain May <efrain_may_75@hotmail.com>
2021-03-27 14:13:25 -0600

    CardHireMe component general at the application

---
+++ lib/src/components/CardHireMe.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:portatofio_efraindrummer_flutterwebb/constantes.dart';
+
+class CardHireMe extends StatelessWidget {
+  const CardHireMe({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(kDefaultPadding * 2),
+      constraints: BoxConstraints(maxWidth: 1110),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [kDefaultShadow]
+      ),
+      child: Row(
+        children: <Widget>[
+          Image.asset(
+            "assets/images/email.png",
+            height: 80,
+            width: 80,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: SizedBox(
+              height: 80, 
+              child: VerticalDivider(),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "comenzando un nuevo proyecto".toUpperCase(), 
+                  style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+                ),
+                SizedBox(height: kDefaultPadding / 2),
+                Text(
+                  "obtenga un presupuesto estimado para el nuevo proyecto".toUpperCase(),
+                  style: TextStyle(fontWeight: FontWeight.w200),
+                )
+              ],
+            ),
+          ),
+          TextButton(
+            onPressed: (){},
+            child: Row(
+              children: <Widget>[
+                Image.asset("assets/images/hand.png"),
+                Text("Contratame!"),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/src/sections/work_recent/work_recent.dart
+++ lib/src/sections/work_recent/work_recent.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/constantes.dart';
+import 'package:portatofio_efraindrummer_flutterwebb/src/components/CardHireMe.dart';
 
 class WorkRecent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(top: kDefaultPadding * 6),
       width: double.infinity,
       height: 600,
       decoration: BoxDecoration(
@@ -17,49 +19,13 @@ class WorkRecent extends StatelessWidget {
       ),
       child: Column(
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(kDefaultPadding * 2),
-            constraints: BoxConstraints(maxWidth: 1110),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [kDefaultShadow]
-            ),
-            child: Row(
-              children: <Widget>[
-                Image.asset(
-                  "assets/images/email.png",
-                  height: 80,
-                  width: 80,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  child: SizedBox(
-                    height: 80, 
-                    child: VerticalDivider(),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "comenzando un nuevo proyecto".toUpperCase(), 
-                        style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
-                      ),
-                      SizedBox(height: kDefaultPadding / 2),
-                      Text(
-                        "obtenga un presupuesto estimado para el nuevo proyecto".toUpperCase(),
-                        style: TextStyle(fontWeight: FontWeight.w200),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
+          Transform.translate(
+            offset: Offset(0, -80),
+            child: CardHireMe()
           )
         ],
       ),
     );
   }
 }
+
