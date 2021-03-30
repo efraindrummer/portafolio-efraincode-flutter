036b5c609fb6f03e908a323cca1f8ddcc77a4d99
Efrain May <efrain_may_75@hotmail.com>
2021-03-30 12:40:33 -0600

    list generate card

--- lib/src/sections/work_recent/work_recent.dart
+++ lib/src/sections/work_recent/work_recent.dart
@@ -30,44 +30,68 @@ class WorkRecent extends StatelessWidget {
             subTitle: "Mis Areas Fuertes",
             color: Color(0xFFFFB100),
           ),
-          Container(
-            height: 320,
-            width: 540,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [kDefaultShadow]
-            ),
-            child: Row(
-              children: <Widget>[
-                Image.asset(trabajosRecientes[0].image),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(trabajosRecientes[0].category.toUpperCase()),
-                        SizedBox(height: kDefaultPadding / 2),
-                        Text(trabajosRecientes[0].title, style: Theme.of(context).textTheme.headline5.copyWith(
-                          height: 1.5
-                        ),),
-                        SizedBox(height: kDefaultPadding),
-                        Text(
-                          "Ver Detalles", 
-                          style: TextStyle(
-                            decoration: TextDecoration.underline
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
+          SizedBox(height: kDefaultPadding * 1.5),
+          SizedBox(
+            width: 1110,
+            child: Wrap(
+              spacing: kDefaultPadding,
+              runSpacing: kDefaultPadding * 2,
+              children: List.generate(trabajosRecientes.length, (index) => RecentCardWidget(index: index)),
             ),
           ),
           SizedBox(height: kDefaultPadding * 5)
+        ],
+      ),
+    );
+  }
+}
+
+class RecentCardWidget extends StatelessWidget {
+  
+  const RecentCardWidget({
+    Key key, 
+    this.index,
+  }) : super(key: key);
+
+  final int index;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      width: 540,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [kDefaultShadow]
+      ),
+      child: Row(
+        children: <Widget>[
+          Image.asset(trabajosRecientes[index].image),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(trabajosRecientes[index].category.toUpperCase()),
+                  SizedBox(height: kDefaultPadding / 2),
+                  Text(trabajosRecientes[index].title, style: Theme.of(context).textTheme.headline5.copyWith(
+                    height: 1.5
+                  ),),
+                  SizedBox(height: kDefaultPadding),
+                  Text(
+                    "Ver Detalles", 
+                    style: TextStyle(
+                      decoration: TextDecoration.underline
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
