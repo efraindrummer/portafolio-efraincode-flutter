738cebeadfb5536075df22dbfb7a40a4de042ce6
Efrain May <efrain_may_75@hotmail.com>
2021-03-30 13:17:24 -0600

    circle user pic section feedback

--- lib/src/sections/feedback/feedback_section.dart
+++ lib/src/sections/feedback/feedback_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/constantes.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/src/components/SectionTitle.dart';
+import 'package:portatofio_efraindrummer_flutterwebb/src/models/Opiniones.dart';
 
 class FeedBackSection extends StatelessWidget {
 
@@ -13,8 +14,36 @@ class FeedBackSection extends StatelessWidget {
         children: <Widget>[
           SectionTitle(
             title: "Recomendaciones Recibidas",
-            subTitle: "Testimonios de clientes satisfechos",
+            subTitle: "Opiniones de clientes satisfechos",
             color: Color(0xFF00B1FF),
+          ),
+          SizedBox(height: kDefaultPadding),
+          Container(
+            height: 350,
+            width: 350,
+            decoration: BoxDecoration(
+              color: opiniones[0].color,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [kDefaultShadow]
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 10,
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage(opiniones[0].userPic)
+                    )
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
