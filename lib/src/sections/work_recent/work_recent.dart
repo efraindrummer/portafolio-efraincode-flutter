5bedf774967743110d9ccd906ab23218a49bef98
Efrain May <efrain_may_75@hotmail.com>
2021-03-30 12:34:07 -0600

    card desing image and flex cross and padding reset

--- lib/src/sections/work_recent/work_recent.dart
+++ lib/src/sections/work_recent/work_recent.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/constantes.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/src/components/CardHireMe.dart';
 import 'package:portatofio_efraindrummer_flutterwebb/src/components/SectionTitle.dart';
+import 'package:portatofio_efraindrummer_flutterwebb/src/models/TrabajosRecientes.dart';
 
 class WorkRecent extends StatelessWidget {
 
@@ -10,7 +11,7 @@ class WorkRecent extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(top: kDefaultPadding * 6),
       width: double.infinity,
-      height: 600,
+      //height: 600,
       decoration: BoxDecoration(
         color: Colors.black.withOpacity(0.3),
         image: DecorationImage(
@@ -28,7 +29,45 @@ class WorkRecent extends StatelessWidget {
             title: "Trabajos Recientes",
             subTitle: "Mis Areas Fuertes",
             color: Color(0xFFFFB100),
-          )
+          ),
+          Container(
+            height: 320,
+            width: 540,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [kDefaultShadow]
+            ),
+            child: Row(
+              children: <Widget>[
+                Image.asset(trabajosRecientes[0].image),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(trabajosRecientes[0].category.toUpperCase()),
+                        SizedBox(height: kDefaultPadding / 2),
+                        Text(trabajosRecientes[0].title, style: Theme.of(context).textTheme.headline5.copyWith(
+                          height: 1.5
+                        ),),
+                        SizedBox(height: kDefaultPadding),
+                        Text(
+                          "Ver Detalles", 
+                          style: TextStyle(
+                            decoration: TextDecoration.underline
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: kDefaultPadding * 5)
         ],
       ),
     );
