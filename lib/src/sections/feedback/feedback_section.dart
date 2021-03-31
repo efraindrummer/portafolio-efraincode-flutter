8b583833e8b27563d8732cada5878591c7e958e6
Efrain May <efrain_may_75@hotmail.com>
2021-03-31 12:05:02 -0600

    section feddback card lista

--- lib/src/sections/feedback/feedback_section.dart
+++ lib/src/sections/feedback/feedback_section.dart
@@ -18,49 +18,70 @@ class FeedBackSection extends StatelessWidget {
             color: Color(0xFF00B1FF),
           ),
           SizedBox(height: kDefaultPadding),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            height: 350,
-            width: 350,
-            decoration: BoxDecoration(
-              color: opiniones[0].color,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [kDefaultShadow]
-            ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 10,
-                    ),
-                    image: DecorationImage(
-                      image: AssetImage(opiniones[0].userPic)
-                    )
-                  ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(opiniones.length, (index) => FeedbackCard(index: index,)),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FeedbackCard extends StatelessWidget {
+  
+  const FeedbackCard({
+    Key key, this.index,
+  }) : super(key: key);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: kDefaultPadding * 3),
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      height: 350,
+      width: 350,
+      decoration: BoxDecoration(
+        color: opiniones[index].color,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [kDefaultShadow]
+      ),
+      child: Column(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(0, -55),
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 10,
                 ),
-                Text(
-                  opiniones[0].review, 
-                  style: TextStyle(
-                    color: kTextColor, 
-                    fontSize: 18, 
-                    fontWeight: FontWeight.w200,
-                    fontStyle: FontStyle.italic,
-                    height: 1.5
-                  )
-                ),
-                SizedBox(height: kDefaultPadding * 2),
-                Text(
-                  "Ronald Thompson", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
+                image: DecorationImage(
+                  image: AssetImage(opiniones[index].userPic)
                 )
-              ],
+              ),
+            ),
+          ),
+          Text(
+            opiniones[index].review, 
+            style: TextStyle(
+              color: kTextColor, 
+              fontSize: 18, 
+              fontWeight: FontWeight.w200,
+              fontStyle: FontStyle.italic,
+              height: 1.5
+            )
+          ),
+          SizedBox(height: kDefaultPadding * 2),
+          Text(
+            "Ronald Thompson", 
+            style: TextStyle(
+              fontWeight: FontWeight.bold
             ),
           )
         ],
