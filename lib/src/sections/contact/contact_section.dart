9e8541dd04afe95535292982cf787dafead9274a
Efrain May <efrain_may_75@hotmail.com>
2021-03-31 12:56:34 -0600

    fittedbox in the created SocialCardWidget separate

--- lib/src/sections/contact/contact_section.dart
+++ lib/src/sections/contact/contact_section.dart
@@ -7,7 +7,7 @@ class ContactSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
+      //height: 500,
       width: double.infinity,
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -26,21 +26,68 @@ class ContactSection extends StatelessWidget {
             subTitle: "Consulta e Informate sobre Proyectos",
             color: Color(0xFF00d2ff),
           ),
-          Container(
-            constraints: BoxConstraints(
-              maxWidth: 1110
-            ),
-            padding: EdgeInsets.all(kDefaultPadding * 3),
-            margin: EdgeInsets.only(top: kDefaultPadding * 2),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20), 
-                topRight: Radius.circular(20) 
-              )
-            ),
-          )
+          ContactBox()
         ],
+      ),
+    );
+  }
+}
+
+class ContactBox extends StatelessWidget {
+  const ContactBox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: 1110
+      ),
+      padding: EdgeInsets.all(kDefaultPadding * 3),
+      margin: EdgeInsets.only(top: kDefaultPadding * 2),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20), 
+          topRight: Radius.circular(20) 
+        )
+      ),
+      child: Column(
+        children: <Widget>[
+          SocialCardWidget()
+        ],
+      ),
+    );
+  }
+}
+
+class SocialCardWidget extends StatelessWidget {
+  const SocialCardWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2, horizontal: kDefaultPadding * 1.5),
+        decoration: BoxDecoration(
+          color: Color(0xFF5433FF),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [kDefaultShadow]
+        ),
+        child: Row(
+          children: <Widget>[
+            Image.asset(
+              "assets/images/skype.png",
+              height: 80,
+              width: 80,
+            ),
+            SizedBox(width: kDefaultPadding),
+            Text("@efraincode")
+          ],
+        ),
       ),
     );
   }
